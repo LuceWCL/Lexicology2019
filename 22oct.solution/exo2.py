92450f53c627747d43295909ba42d21ac3c48af0
LucWCL <lucie.warckol@gmail.com>
2019-11-13 16:20:38 +0100

    update

--- 22oct.solution/exo2.py
+++ 22oct.solution/exo2.py
@@ -6,7 +6,7 @@ def main():
   x = 1
   A = np.zeros([15,10])
   for i in range(0,A.shape[0]) :
-    for j in range(0,A.shape[1]) : 
+    for j in range(0,A.shape[1]) :
       A[i,j] = x
       x = x+2
   print(A)
@@ -14,20 +14,20 @@ def main():
 # sans boucle
   A = np.arange(1,300,2).reshape(15,10)
   print(A)
-  
+
   B = A[:,2:6]
   print(B)
-  
+
   C = A[0:5]
   print(C)
 
-  z = np.zeros(10)  
+  z = np.zeros(10)
   z[4] = 1
   print(z)
-  
+
   p = np.dot(A[0],z)
   print(p)
-  
+
   q = np.dot(C,z)
   print(q)
 
